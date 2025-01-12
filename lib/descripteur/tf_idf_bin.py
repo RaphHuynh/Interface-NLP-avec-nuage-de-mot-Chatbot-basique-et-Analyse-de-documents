00f421abec2c0c11e29cce25e911ffb76caea005
Raphhuynh <raphaelle.huynh@outlook.fr>
2025-01-12 23:11:17 +0100

    update: tf-idf-bin

--- lib/descripteur/tf_idf_bin.py
+++ lib/descripteur/tf_idf_bin.py
@@ -1,17 +1,38 @@
 import math
 from typing import List
+from lib.spacy_nltk.stemming import *
 
-def tf_idf_bin(corpus: List[str],list_mot: List[str], stemming: int) -> List[List[float]]:
+def tf_idf_bin(corpus: List[str], list_mot: List[str], stemming: str) -> List[List[float]]:
     """
     Calcule la matrice TF-IDF binaire pour un corpus donné.
 
     Args:
         corpus (List[str]): Le corpus de documents.
         list_mot (List[str]): La liste des mots du corpus.
+        stemming (str): Option pour choisir le stemming (1 -> Porter, 2 -> Snowball, 3 -> Lancaster, 4 -> Lemmatization, 5 -> Lovins, 6 -> Aucun stemming)
 
     Returns:
         List[List[float]]: La matrice TF-IDF binaire.
     """
+    
+    # Fonction de stemming en fonction de l'option
+    def appliquer_stemming(phrase: str, stemming: str) -> List[str]:
+        phrase = phrase.lower()
+
+        if stemming == "1":
+            return porter_stemmer_phrase(phrase)
+        elif stemming == "2":
+            return snowball_stemmer_phrase(phrase)
+        elif stemming == "3":
+            return lancaster_stemmer_phrase(phrase)
+        elif stemming == "4":
+            return wordnet_lemmatizer_phrase(phrase)
+        elif stemming == "5":
+            return lovins_stemmer(phrase)
+        else:
+            # Pas de stemming appliqué, on garde les mots d'origine
+            return phrase.split()
+
     # Initialiser la matrice TF-IDF
     matrix_tf_idf = [[0 for _ in list_mot] for _ in corpus]
 
@@ -23,8 +44,8 @@ def tf_idf_bin(corpus: List[str],list_mot: List[str], stemming: int) -> List[Lis
 
     # Calculer la matrice TF-IDF
     for i in range(len(corpus)):
-        # Découper le document en mots
-        mots_document = set(corpus[i].split())  # Utiliser un ensemble pour une recherche rapide
+        # Appliquer le stemming ou non selon l'option
+        mots_document = appliquer_stemming(corpus[i], stemming)
 
         for j, mot in enumerate(list_mot):
             # Calculer le terme TF binaire
